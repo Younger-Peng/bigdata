707602dd16df9ad55c0d082d62bfc21602e4b17e
pengyang <pengyang@iynxiao.com>
2017-10-13 16:07:00 +0800

    添加切换页面的功能

--- App.js
+++ App.js
@@ -7,7 +7,8 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import _ from 'lodash';
 import Page from "./components/Page.jsx";
-import './master.css'
+import './master.css';
+import './fonts/iconfont.css'
 
 class Demo extends Component {
     constructor(props) {
@@ -16,8 +17,19 @@ class Demo extends Component {
         this.state = {
             marginTop: 0,
             fullScreen: false,
-            pages: [1, 2, 3, 4, 5, 6, 7]
+            pages: [1, 2, 3, 4, 5, 6, 7],
+            currentPage: 1
         }
+        this.initEvent()
+    }
+
+    initEvent() {
+        document.addEventListener('webkitfullscreenchange', () => {
+            this.setState({
+                ...this.state,
+                fullScreen: !this.state.fullScreen
+            })
+        })
     }
 
     toggleFullScreen() {
@@ -38,24 +50,46 @@ class Demo extends Component {
               elem.webkitRequestFullscreen();
             }
         }
-        this.screenHeight = window.innerHeight
 
+
+    }
+
+    goNextPage() {
+        this.screenHeight = window.innerHeight
         this.setState({
             ...this.state,
-            fullScreen: !this.state.fullScreen
+            marginTop: this.state.marginTop - this.screenHeight,
+            currentPage: this.state.currentPage + 1
+        })
+    }
+
+    goPrePage() {
+        this.screenHeight = window.innerHeight
+        debugger;
+        this.setState({
+            ...this.state,
+            marginTop: this.state.marginTop + this.screenHeight,
+            currentPage: this.state.currentPage - 1
         })
     }
 
     render() {
         return (
-            <div id="container" style={{marginTop: this.state.marginTop, transition: 'margin-top 2s ease' }}>
+            <div id="container" style={{marginTop: this.state.marginTop }}>
                 {
                     _.map(this.state.pages, (page, index) => {
                         return <Page key={`Page${page}`} content={`Page${page}`} pageNum={page} />
                     })
                 }
-                <button style={{position: 'fixed', bottom: 20, right: 20, padding: '5px 10px', borderRadius: '5px', background: 'transparent', borderStyle: 'none', cursor: 'pointer', outline: 'none', fontSize: 14}}  onClick={this.toggleFullScreen.bind(this)}>{this.state.fullScreen ? '退出全屏' : '全屏'}</button>
-
+                {
+                    this.state.fullScreen ? null : <button style={{position: 'fixed', bottom: 20, right: 20, padding: '5px 10px', borderRadius: '5px', background: 'transparent', borderStyle: 'none', cursor: 'pointer', outline: 'none', fontSize: 14}}  onClick={this.toggleFullScreen.bind(this)}><i style={{fontSize: 30, color: '#777'}} className='icon iconfont icon-full-screen'></i></button>
+                }
+                {
+                    this.state.fullScreen && 1 < this.state.currentPage ? <i onClick={this.goNextPage.bind(this)} className='icon iconfont icon-down' style={{position: 'fixed', top: 30, left: '50%', transform: 'translateX(-50%) rotate(180deg)', fontSize: 30, color: '#555', cursor: 'pointer'}} onClick={this.goPrePage.bind(this)}></i> : null
+                }
+                {
+                    this.state.fullScreen && this.state.currentPage < this.state.pages.length ? <i onClick={this.goNextPage.bind(this)} className='icon iconfont icon-down' style={{position: 'fixed', bottom: 30, left: '50%', transform: 'translateX(-50%)', fontSize: 30, color: '#555', cursor: 'pointer'}} onClick={this.goNextPage.bind(this)}></i> : null
+                }
             </div>
         );
     }
